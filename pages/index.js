65bec0849e70073b35141e05a5d75781535d63b9
Zack Hsi <zackhsi@gmail.com>
2020-06-28 21:38:32 +0800

    Milkweed factoid

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,20 @@ export default function Home() {
   return (
     <Layout>
       <h1>RNGY</h1>
+      <blockquote>
+        <p>
+          <i>
+            The number of eggs laid by a female [milkweed butterfly], who may
+            mate several times, ranges from 290 to 1180.
+          </i>
+        </p>
+        <p>
+          —{" "}
+          <a href="https://en.wikipedia.org/wiki/Monarch_butterfly">
+            Wikipedia
+          </a>
+        </p>
+      </blockquote>
     </Layout>
   );
 }
