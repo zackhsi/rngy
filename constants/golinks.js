e743cd60569c2a5c4ab28d4240f99d8da7964bc4
Zack Hsi <zackhsi@gmail.com>
2020-09-27 22:14:27 +0800

    Add creamier golinks

--- constants/golinks.js
+++ constants/golinks.js
@@ -1,4 +1,20 @@
 const GOLINKS = {
-  milkweed: "https://ronggy.com/originals/milkweed",
+  "the-common-rose": "https://ronggy.com/creamier-x-ronggy/the-common-rose",
+  "the-common-tiger": "https://ronggy.com/creamier-x-ronggy/the-common-tiger",
+  "the-blue-spotted-crow":
+    "https://ronggy.com/creamier-x-ronggy/the-blue-spotted-crow",
+  "in-the-limelight": "https://ronggy.com/creamier-x-ronggy/in-the-limelight",
+  "the-blue-pansy": "https://ronggy.com/creamier-x-ronggy/the-blue-pansy",
+  "the-common-posy": "https://ronggy.com/creamier-x-ronggy/the-common-posy",
+  "the-commander": "https://ronggy.com/creamier-x-ronggy/the-commander",
+  "the-fivebar-swordtail":
+    "https://ronggy.com/creamier-x-ronggy/the-fivebar-swordtail",
+  "butterfly-mimicry": "https://ronggy.com/creamier-x-ronggy/butterfly-mimicry",
+  "the-feast-of-the-papilionidae":
+    "https://ronggy.com/creamier-x-ronggy/the-feast-of-the-papilionidae",
+  "the-passion-of-nymphalidae":
+    "https://ronggy.com/creamier-x-ronggy/the-passion-of-nymphalidae",
+  "two-albatrosses-and-a-jezebel":
+    "https://ronggy.com/creamier-x-ronggy/two-albatrosses-and-a-jezebel",
 };
 export default GOLINKS;
