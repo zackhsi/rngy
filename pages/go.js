5bb0faac9a6e947cc1221855a4bc711856bcd544
Zack Hsi <zackhsi@gmail.com>
2020-10-01 14:08:36 +0800

    Fix warnings

--- pages/go.js
+++ pages/go.js
@@ -7,24 +7,28 @@ const Golink = () => {
   return (
     <Layout title="Golinks">
       <table>
-        <tr>
-          <th>Go Link</th>
-          <th>Target</th>
-        </tr>
-        {Object.keys(GOLINKS).map(function (key, index) {
-          return (
-            <tr>
-              <td>
-                <Link href={`/go/${key}`}>
-                  <a>{key}</a>
-                </Link>
-              </td>
-              <td>
-                <a href={GOLINKS[key]}>{GOLINKS[key]}</a>
-              </td>
-            </tr>
-          );
-        })}
+        <thead>
+          <tr>
+            <th>Go Link</th>
+            <th>Target</th>
+          </tr>
+        </thead>
+        <tbody>
+          {Object.keys(GOLINKS).map(function (key, index) {
+            return (
+              <tr key={key}>
+                <td>
+                  <Link href={`/go/${key}`}>
+                    <a>{key}</a>
+                  </Link>
+                </td>
+                <td>
+                  <a href={GOLINKS[key]}>{GOLINKS[key]}</a>
+                </td>
+              </tr>
+            );
+          })}
+        </tbody>
       </table>
     </Layout>
   );
