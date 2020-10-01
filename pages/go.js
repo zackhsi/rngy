cffe95e58d954bda191d2f74a51a7c3221f62138
Zack Hsi <zackhsi@gmail.com>
2020-10-01 16:15:49 +0800

    Implement QR codes

--- pages/go.js
+++ pages/go.js
@@ -1,29 +1,52 @@
+import React, { useEffect, useState } from "react";
+
 import Link from "next/link";
 
 import GOLINKS from "../constants/golinks";
 import Layout from "../components/layout";
 
+const QRCode = require("qrcode");
+
 const Golink = () => {
+  const [qrCodes, setQrCodes] = useState({});
+  useEffect(() => {
+    for (const [shortLink, _longLink] of Object.entries(GOLINKS)) {
+      if (!(shortLink in qrCodes)) {
+        QRCode.toDataURL(`https://rngy.dev/go/${shortLink}`, function (
+          err,
+          url
+        ) {
+          if (err) throw err;
+
+          setQrCodes((qrCodes) => ({ ...qrCodes, [shortLink]: url }));
+        });
+      }
+    }
+  }, []);
   return (
     <Layout title="Golinks">
       <table>
         <thead>
           <tr>
-            <th>Go Link</th>
-            <th>Target</th>
+            <th>Short Link</th>
+            <th>Long Link</th>
+            <th>QR Code</th>
           </tr>
         </thead>
         <tbody>
-          {Object.keys(GOLINKS).map(function (key, index) {
+          {Object.entries(GOLINKS).map(function ([shortLink, longLink], _i) {
             return (
-              <tr key={key}>
+              <tr key={shortLink}>
                 <td>
-                  <Link href={`/go/${key}`}>
-                    <a>{key}</a>
+                  <Link href={`/go/${shortLink}`}>
+                    <a>{shortLink}</a>
                   </Link>
                 </td>
                 <td>
-                  <a href={GOLINKS[key]}>{GOLINKS[key]}</a>
+                  <a href={longLink}>{longLink}</a>
+                </td>
+                <td>
+                  <img src={qrCodes[shortLink]} />
                 </td>
               </tr>
             );
