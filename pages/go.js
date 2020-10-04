f05eac0da3f2d3547f9d363606689b0aba4e3001
Zack Hsi <zackhsi@gmail.com>
2020-10-04 22:36:19 +0800

    Add logo to QR codes

--- pages/go.js
+++ pages/go.js
@@ -1,23 +1,23 @@
 import React, { useEffect, useState } from "react";
 
-import Link from "next/link";
-
 import GOLINKS from "../constants/golinks";
 import Layout from "../components/layout";
+import Link from "next/link";
 
 const QRCode = require("qrcode");
+const { createCanvas, loadImage } = require("canvas");
 
 const Golink = () => {
   const [qrCodes, setQrCodes] = useState({});
   useEffect(() => {
     for (const [shortLink, _longLink] of Object.entries(GOLINKS)) {
       if (!(shortLink in qrCodes)) {
-        QRCode.toDataURL(`https://rngy.dev/go/${shortLink}`, function (
-          err,
-          url
-        ) {
-          if (err) throw err;
-
+        create_qrcode(
+          `https://rngy.dev/go/${shortLink}`,
+          "./ronggy.png",
+          800,
+          150
+        ).then((url) => {
           setQrCodes((qrCodes) => ({ ...qrCodes, [shortLink]: url }));
         });
       }
@@ -56,5 +56,54 @@ const Golink = () => {
     </Layout>
   );
 };
+
+async function create_qrcode(
+  qrCodeText,
+  centerImageSrc,
+  qrCodeWidth,
+  centerImageWidth
+) {
+  // Create canvas.
+  const canvas = createCanvas(qrCodeWidth, qrCodeWidth);
+  const ctx = canvas.getContext("2d");
+
+  // Draw QR code.
+  const qrCodeImage = await QRCode.toCanvas(createCanvas(1, 1), qrCodeText, {
+    errorCorrectionLevel: "H", // LMQH, "H" is highest.
+    margin: 1,
+    color: {
+      dark: "#000000",
+      light: "#ffffff",
+    },
+  });
+  ctx.drawImage(qrCodeImage, 0, 0, qrCodeWidth, qrCodeWidth);
+
+  // Draw circle.
+  ctx.beginPath();
+  const paddingFactor = 2; // Fancy restaurants use big plates for small food.
+  ctx.arc(
+    qrCodeWidth / 2,
+    qrCodeWidth / 2,
+    (centerImageWidth / 2) * paddingFactor,
+    0,
+    2 * Math.PI
+  );
+  ctx.strokeStyle = "white";
+  ctx.stroke();
+  ctx.fillStyle = "white";
+  ctx.fill();
+
+  // Draw image.
+  ctx.drawImage(
+    await loadImage(centerImageSrc),
+    (qrCodeWidth - centerImageWidth) / 2,
+    (qrCodeWidth - centerImageWidth) / 2,
+    centerImageWidth,
+    centerImageWidth
+  );
+
+  // Return.
+  return canvas.toDataURL("image/png");
+}
 
 export default Golink;
