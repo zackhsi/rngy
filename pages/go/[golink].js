c35d4c79ad62bcfb1f4adb77df64b0d0d5ca493c
Zack Hsi <zackhsi@gmail.com>
2020-10-14 10:58:11 +0800

    Remove CSS from golinks

--- pages/go/[golink].js
+++ pages/go/[golink].js
@@ -1,9 +1,9 @@
-import { useEffect } from "react";
+import { GTM_AUTH, GTM_ID, GTM_PREVIEW } from "../../constants/gtm";
 
-import { useRouter } from "next/router";
-
-import Layout from "../../components/layout";
 import GOLINKS from "../../constants/golinks";
+import Head from "next/head";
+import { useEffect } from "react";
+import { useRouter } from "next/router";
 
 const DEFAULT_GOLINK = "https://ronggy.com/";
 
@@ -14,9 +14,29 @@ const Golink = () => {
     window.location.replace(GOLINKS[golink] || DEFAULT_GOLINK);
   });
   return (
-    <Layout title={"RNGY → " + golink}>
+    <div>
+      <Head>
+        <title>{"RNGY → " + golink}</title>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl+ '&gtm_auth=${GTM_AUTH}&gtm_preview=${GTM_PREVIEW}&gtm_cookies_win=x';f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','${GTM_ID}');`,
+          }}
+        />
+      </Head>
+      <noscript
+        dangerouslySetInnerHTML={{
+          __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=${GTM_ID}&gtm_auth=${GTM_AUTH}&gtm_preview=${GTM_PREVIEW}&gtm_cookies_win=x"
+            height="0" width="0" style="display:none;visibility:hidden"></iframe`,
+        }}
+      />
       <p>Redirecting...</p>
-    </Layout>
+    </div>
   );
 };
 
