664a8454a916a217da72e84808d2c1404066ad87
Zack Hsi <zackhsi@gmail.com>
2020-11-13 15:34:34 +0800

    Update the rest of the links

--- constants/golinks.js
+++ constants/golinks.js
@@ -1,26 +1,31 @@
 const GOLINKS = {
   "creamier-x-ronggy": "https://ronggy.com/butterfliesofsingapore",
-  "the-common-jay": "https://ronggy.com/creamier-x-ronggy/the-common-jay",
-  "the-common-rose": "https://ronggy.com/creamier-x-ronggy/the-common-rose",
-  "the-common-tiger": "https://ronggy.com/creamier-x-ronggy/the-common-tiger",
+  "the-common-jay": "https://ronggy.com/butterfliesofsingapore/the-common-jay",
+  "the-common-rose":
+    "https://ronggy.com/butterfliesofsingapore/the-common-rose",
+  "the-common-tiger":
+    "https://ronggy.com/butterfliesofsingapore/the-common-tiger",
   "the-blue-spotted-crow":
-    "https://ronggy.com/creamier-x-ronggy/the-blue-spotted-crow",
-  "in-the-limelight": "https://ronggy.com/creamier-x-ronggy/in-the-limelight",
+    "https://ronggy.com/butterfliesofsingapore/the-blue-spotted-crow",
+  "in-the-limelight":
+    "https://ronggy.com/butterfliesofsingapore/in-the-limelight",
   "the-common-tree-nymph":
-    "https://ronggy.com/creamier-x-ronggy/the-common-tree-nymph",
-  "the-blue-pansy": "https://ronggy.com/creamier-x-ronggy/the-blue-pansy",
-  "the-common-posy": "https://ronggy.com/creamier-x-ronggy/the-common-posy",
-  "the-commander": "https://ronggy.com/creamier-x-ronggy/the-commander",
+    "https://ronggy.com/butterfliesofsingapore/the-common-tree-nymph",
+  "the-blue-pansy": "https://ronggy.com/butterfliesofsingapore/the-blue-pansy",
+  "the-common-posy":
+    "https://ronggy.com/butterfliesofsingapore/the-common-posy",
+  "the-commander": "https://ronggy.com/butterfliesofsingapore/the-commander",
   "the-fivebar-swordtail":
-    "https://ronggy.com/creamier-x-ronggy/the-fivebar-swordtail",
-  "butterfly-mimicry": "https://ronggy.com/creamier-x-ronggy/butterfly-mimicry",
+    "https://ronggy.com/butterfliesofsingapore/the-fivebar-swordtail",
+  "butterfly-mimicry":
+    "https://ronggy.com/butterfliesofsingapore/butterfly-mimicry",
   "the-feast-of-the-papilionidae":
-    "https://ronggy.com/creamier-x-ronggy/the-feast-of-the-papilionidae",
+    "https://ronggy.com/butterfliesofsingapore/the-feast-of-the-papilionidae",
   "the-passion-of-nymphalidae":
-    "https://ronggy.com/creamier-x-ronggy/the-passion-of-nymphalidae",
+    "https://ronggy.com/butterfliesofsingapore/the-passion-of-nymphalidae",
   "two-albatrosses-and-a-jezebel":
-    "https://ronggy.com/creamier-x-ronggy/two-albatrosses-and-a-jezebel",
+    "https://ronggy.com/butterfliesofsingapore/two-albatrosses-and-a-jezebel",
   "the-malay-staff-sergeant":
-    "https://ronggy.com/creamier-x-ronggy/the-malay-staff-sergeant",
+    "https://ronggy.com/butterfliesofsingapore/the-malay-staff-sergeant",
 };
 export default GOLINKS;
